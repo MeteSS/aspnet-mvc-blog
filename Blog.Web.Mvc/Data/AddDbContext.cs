6462bbd8a772a44fec33f94382e50d7da7812562
MeteSS <127654684+MeteSS@users.noreply.github.com>
2023-06-16 21:28:55 +0300

    yeni güncelleme

---
+++ Blog.Web.Mvc/Data/AddDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Mvc.Data
+{
+    public class AddDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-CQBBURH\\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Database=Deneme;");
+      // farklı bende bağlantı
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+
+}
+
